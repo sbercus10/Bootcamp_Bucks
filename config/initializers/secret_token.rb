233e1f64a60f68562485a811dbeb6a16af1b6a86
Shawn Bercuson <shawn.bercuson@gmail.com>
2014-03-04 13:45:16 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootcampBucks::Application.config.secret_token = '78f608ffe16eab0dbcd96dc52f87a1d0f8db3983039da4a5df7d6a84a6131b6dc3ddb3563c72145f5676ddf31ff7604313db4175d2b769978c3e2016309ed74c'
