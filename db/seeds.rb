dc6935ba5235cf20896ba96b0a03d7fbae1e7b09
Rebecca Nguyen <onguyen89@gmail.com>
2014-03-15 15:28:34 -0600

    Changes seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,60 +17,30 @@
 # puts 'user: ' << user.name
 # user.add_role :admin
 
-    Bootcamp.create(name:"AppAcademy", city: "San Francisco, New York", down_payment:"$3375", length:
-    Bootcamp.create(name:"Bitmaker Labs", city: "Toronto", down_payment:"$2250", length:
-    Bootcamp.create(name:"Code Fellows", city: "Seattle", down_payment:"$1250", length:
-    Bootcamp.create(name:"Coder Camps ", city: "Houston, TX", down_payment:"$2225", length:
-    Bootcamp.create(name:"Coding Dojo", city: "San Francisco", down_payment:"$2187.5", length:
-    Bootcamp.create(name:"Craftsmanship Academy ", city: "Holly Springs, North Carolina", down_payment:"$1750", length:
-    Bootcamp.create(name:"DaVinci Coders", city: "Boulder, CO", down_payment:"$1500", length:
-    Bootcamp.create(name:"DevBootcamp", city: "San Francisco, Chicago", down_payment:"$3000", length:
-    Bootcamp.create(name:"DevMountain", city: "Ogden, Salt Lake City", down_payment:"$975", length:
-    Bootcamp.create(name:"DevPoint Labs", city: "Salt Lake City, Las Vegas", down_payment:"$2125", length:
-    Bootcamp.create(name:"Disruption Institute", city: "Kansas City", down_payment:"$1500", length:
-    Bootcamp.create(name:"FlatIron School", city: "New York", down_payment:"$3000", length:
-    Bootcamp.create(name:"FullStack Academy", city: "New York", down_payment:"$3000", length:
-    Bootcamp.create(name:"G School", city: "Denver", down_payment:"$5000", length:
-    Bootcamp.create(name:"General Assembly", city: "New York", down_payment:"$2875", length:
-    Bootcamp.create(name:"Hack Bright Academy", city: "San Francisco", down_payment:"$3000", length:
-    Bootcamp.create(name:"Hack Bright Academy", city: "San Francisco", down_payment:"2500", length:
-    Bootcamp.create(name:"Hack Reactor", city: "San Francisco", down_payment:"$4445", length:
-    Bootcamp.create(name:"Hacker You", city: "Toronto", down_payment:"$700", length:
-    Bootcamp.create(name:"Launch Academy", city: "New York, Boston", down_payment:"$3875", length:
-    Bootcamp.create(name:"Maker Square", city: " Austin, TX", down_payment:"$2250", length:
-    Bootcamp.create(name:"Mobile Makers", city: "Chicago", down_payment:"$1750", length:
-    Bootcamp.create(name:"RefactorU", city: "Boulder", down_payment:"$3375", length:
-    Bootcamp.create(name:"Software Craftsmanship Guild", city: "Acron", down_payment:"$2500", length:
-    Bootcamp.create(name:"Starter League ", city: "Chicago", down_payment:"$2000", length:
-    Bootcamp.create(name:"Starter School ", city: "Chicago", down_payment:"$1625", length:
-    Bootcamp.create(name:"Startup Institute", city: "Boston, New York, Chicago", down_payment:"$1312.5", length:
-
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
-Bootcamp.create(name:"", city: "", down_payment:"", length:"", cost:"")
+    Bootcamp.create(name:"AppAcademy", city: "San Francisco, New York", down_payment:"$3375", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"Bitmaker Labs", city: "Toronto", down_payment:"$2250", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"Code Fellows", city: "Seattle", down_payment:"$1250", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"Coder Camps ", city: "Houston, TX", down_payment:"$2225", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"Coding Dojo", city: "San Francisco", down_payment:"$2187.5", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"Craftsmanship Academy ", city: "Holly Springs, North Carolina", down_payment:"$1750", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"DaVinci Coders", city: "Boulder, CO", down_payment:"$1500", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"DevBootcamp", city: "San Francisco, Chicago", down_payment:"$3000", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"DevMountain", city: "Ogden, Salt Lake City", down_payment:"$975", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"DevPoint Labs", city: "Salt Lake City, Las Vegas", down_payment:"$2125", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"Disruption Institute", city: "Kansas City", down_payment:"$1500", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"FlatIron School", city: "New York", down_payment:"$3000", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"FullStack Academy", city: "New York", down_payment:"$3000", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"G School", city: "Denver", down_payment:"$5000", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"General Assembly", city: "New York", down_payment:"$2875", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"Hack Bright Academy", city: "San Francisco", down_payment:"$3000", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"Hack Bright Academy", city: "San Francisco", down_payment:"2500", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"Hack Reactor", city: "San Francisco", down_payment:"$4445", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"Hacker You", city: "Toronto", down_payment:"$700", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"Launch Academy", city: "New York, Boston", down_payment:"$3875", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"Maker Square", city: " Austin, TX", down_payment:"$2250", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"Mobile Makers", city: "Chicago", down_payment:"$1750", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"RefactorU", city: "Boulder", down_payment:"$3375", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"Software Craftsmanship Guild", city: "Acron", down_payment:"$2500", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"Starter League ", city: "Chicago", down_payment:"$2000", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"Starter School ", city: "Chicago", down_payment:"$1625", length: "3 weeks", tuition_cost: "$5")
+    Bootcamp.create(name:"Startup Institute", city: "Boston, New York, Chicago", down_payment:"$1312.5", length: "3 weeks", tuition_cost: "$5")
