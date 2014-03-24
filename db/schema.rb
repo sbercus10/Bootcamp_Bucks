cc014f016da7e66e350a795bac84344c88b625f7
Rebecca Nguyen <onguyen89@gmail.com>
2014-03-24 16:41:16 -0600

    Runs bootcamp id migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140324184823) do
+ActiveRecord::Schema.define(:version => 20140324220219) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(:version => 20140324184823) do
     t.integer  "loan_length"
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
+    t.string   "bootcamp"
     t.integer  "down_payment"
     t.integer  "total_payment_due"
     t.integer  "true_cost"
