9e1678843fe1cc7b09fbeaf0026554a41468d2a1
Shawn Bercuson <shawn.bercuson@gmail.com>
2014-03-27 15:45:10 -0600

    Adds precompiled assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,11 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-// = require jquery
-// = require jquery_ujs
-// = require jquery.ui.all
-// = require bootstrap
-// = require_tree .
+//= require jquery
+//= require jquery_ujs
+//= require jquery.ui.all
+//= require bootstrap
+//= require active_admin/base
+//= require_tree .
 
 
